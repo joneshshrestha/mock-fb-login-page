b8d4c7cc0b0d87a5adbaced0ab6b6694f9758833
joneshshrestha <contact@joneshshrestha.com>
2020-08-14 15:56:54 +0545

    rollback

--- script.js
+++ script.js
@@ -4,5 +4,4 @@ document.getElementById("login-btn").addEventListener("click", function() {
    username = (document.getElementById('exampleInputEmail1').value)
    password = (document.getElementById('exampleInputPassword1').value)
    console.log(username, password)
-
 })
