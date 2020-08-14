aedd73cfb7afe1623f16576bff843a20f0c93e26
joneshshrestha <contact@joneshshrestha.com>
2020-08-14 11:50:00 +0545

    app.js

---
+++ script.js
@@ -0,0 +1,8 @@
+var username, password
+
+document.getElementById("login-btn").addEventListener("click", function() {
+   username = (document.getElementById('exampleInputEmail1').value)
+   password = (document.getElementById('exampleInputPassword1').value)
+   console.log(username, password)
+
+})
